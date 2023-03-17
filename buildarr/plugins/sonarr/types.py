88df6f3072bbdf248e3f051497b10b6ec0a1ffca
Callum Dickinson <callum.karamu@gmail.com>
2023-03-18 11:47:04 +1300

    Remove unused code and fix pre-commit job

--- buildarr/plugins/sonarr/types.py
+++ buildarr/plugins/sonarr/types.py
@@ -19,12 +19,9 @@ Sonarr plugin type hints.
 
 from __future__ import annotations
 
-from datetime import datetime, timezone
-from http import HTTPStatus
-from typing import Literal, Optional
+from typing import Literal
 
 from pydantic import SecretStr
-from requests import Response
 
 SonarrProtocol = Literal["http", "https"]
 
@@ -36,50 +33,3 @@ class SonarrApiKey(SecretStr):
 
     min_length = 32
     max_length = 32
-
-
-def create_dryrun_response(
-    method: str,
-    url: str,
-    status_code: Optional[int] = None,
-    content_type: str = "application/json",
-    charset: str = "utf-8",
-    content: str = "{}",
-) -> Response:
-    """
-    A utility class for generating `requests.Response` objects in dry-run mode.
-
-    _extended_summary_
-
-    Args:
-        Response (_type_): _description_
-    """
-
-    response = Response()
-    response.url = url
-    response.headers["Vary"] = "Accept"
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, max-age=0"
-    response.headers["Pragma"] = "no-cache"
-    response.headers["Expires"] = "0"
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Content-Type"] = f"{content_type}; charset={charset}"
-    response.headers["Server"] = "Mono-HTTPAPI/1.0"
-    response.headers["Date"] = datetime.now(tz=timezone.utc).strftime("%a, %d %b %Y %H:%M:%S %Z")
-    response.headers["Transfer-Encoding"] = "chunked"
-    if status_code is not None:
-        response.status_code = status_code
-    elif method == "POST":
-        response.status_code = int(HTTPStatus.CREATED)
-    elif method == "PUT":
-        response.status_code = int(HTTPStatus.ACCEPTED)
-    elif method == "DELETE":
-        response.status_code = int(HTTPStatus.OK)
-    else:
-        raise ValueError(
-            f"Unsupported HTTP method for creating dry-run response: {str(method)}",
-        )
-    response.encoding = charset
-    if content is not None:
-        response._content = content.encode("UTF-8")
-
-    return response
